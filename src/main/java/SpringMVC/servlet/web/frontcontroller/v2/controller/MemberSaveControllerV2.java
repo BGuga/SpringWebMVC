8b88537ea79da4bdfc2a04982f2cf2d21b2d25f4
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-05 14:12:41 +0900

    frontcontroll -v2 : controller의 forward역할을 myView로 분산

--- src/main/java/SpringMVC/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/SpringMVC/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -1,8 +1,9 @@
-package SpringMVC.servlet.web.frontcontroller.v1.controller;
+package SpringMVC.servlet.web.frontcontroller.v2.controller;
 
 import SpringMVC.servlet.domain.member.Member;
 import SpringMVC.servlet.domain.member.MemberRepository;
-import SpringMVC.servlet.web.frontcontroller.v1.ControllerV1;
+import SpringMVC.servlet.web.frontcontroller.MyView;
+import SpringMVC.servlet.web.frontcontroller.v2.ControllerV2;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,12 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MemberSaveController implements ControllerV1 {
-
+public class MemberSaveControllerV2 implements ControllerV2 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         int age1 = Integer.parseInt(request.getParameter("age"));
 
@@ -25,8 +25,6 @@ public class MemberSaveController implements ControllerV1 {
         //Model의 데이터를 보관한다
         request.setAttribute("member",member);
 
-        String viewPath = "/WEB-INF/views/save-result.jsp";
-        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request,response);
+        return new MyView("/WEB-INF/views/save-result.jsp");
     }
 }
